736899a94681a9d9b2813e02fd47ee85aee15e71
Edison K <koedison@gmail.com>
2020-01-22 20:27:26 -0800

    add nodejs folder and add nodejs exercise 1

---
+++ nodejs_exercises/index1.js
@@ -0,0 +1,14 @@
+// Exercise 1
+// Create a new index1.js file
+// Declare the following variables:
+// First name
+// Last name
+// Age
+// Address
+// Date of birth (using camel case)
+
+var first_name;
+var last_name;
+var age;
+var address;
+var dateOfBirth;
