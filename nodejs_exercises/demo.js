86fd0aa86aecfb1e83d7ce1fdcdcfa0af9a5d730
Edison K <koedison@gmail.com>
2020-01-24 20:30:02 -0800

    add demo.js and index14.js files

---
+++ nodejs_exercises/demo.js
@@ -0,0 +1,49 @@
+// Random exercise class demos
+
+// --let
+// let name = 'Jane';
+// let age = 31;
+
+// console.log(name + ' is ' + age + ' old. ')
+
+
+// --var
+// var name = 'john';
+// console.log (name)
+
+// name = 'paul'
+// console.log (name)
+
+
+// --let
+// let name = 'john';
+// console.log (name)
+
+// name = 'paul'
+// console.log (name)
+
+
+// --const never change compared to var and let
+// const name = 'john';
+// console.log (name)
+
+// name = 'paul'
+// console.log (name)
+
+
+// --boolean and number together
+// let active = true
+// let doorCode = 1234
+
+// console.log ('what is the door code? ' + doorCode + active, typeof(doorCode))
+
+
+// --template literals
+let name = 'Bruce Wayne';
+// first two methods
+console.log('welcome ' + name)
+console.log('welcome %s', name)
+
+// template leteral method
+console.log(`welcome ${name}`)
+console.log(`welcome ${name} \nHave a nice day`)
---
+++ nodejs_exercises/index14.js
@@ -0,0 +1,6 @@
+let firstName = 'Emerson'
+let greeting = 'welcome'
+let endOfMessage = 'to learning JavaScript with friends'
+
+console.log('%s %s %s', greeting, firstName, endOfMessage)
+console.log('%s %s', greeting, firstName + endOfMessage)
