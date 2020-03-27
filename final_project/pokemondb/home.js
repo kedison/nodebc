ea3d8a5f240184a11f7c2b09df6d036542c99504
Edison K <koedison@gmail.com>
2020-03-26 22:56:27 -0700

    add css, home, index files

--- final_project/pokemondb/home.js
+++
@@ -1,3 +0,0 @@
-const home = (req, res) => res.send('Hello from home page');
-
-module.exports = home;
--- final_project/pokemondb/index.js
+++ final_project/pokemondb/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// middlewares
+app.set('view engine', 'pug')
+app.use(express.static('public'))
 
 // modules
-const homex = require ('./home')
+const homev = require ('./routes/home')
 
-app.get('/', homex)
+app.get('/', homev)
 
 app.listen(port, () => console.log(`Pokemon database running on port ${port}!`))
