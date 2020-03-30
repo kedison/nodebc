99e0c1d8fc8e9eec285851ce134a84eaa8d36995
Edison K <koedison@gmail.com>
2020-03-29 18:27:23 -0700

    making home an array of objects

--- final_project/pokemondb/routes/home.js
+++ final_project/pokemondb/routes/home.js
@@ -1,5 +1,43 @@
 const homev = (req, res) => {
-    res.render('home')
+
+    const pokemondbArray = [{
+            id: 1,
+            number: "001",
+            name: "Bulbasaur",
+            type: "Grass / Poison",
+            hp: 45,
+            attack: 49,
+            defense: 49
+        },
+        {
+            id: 2,
+            number: "012",
+            name: "Ivysaur",
+            type: "Grass / Poison",
+            hp: 60,
+            attack: 62,
+            defense: 63
+        },
+        {
+            id: 3,
+            number: "003",
+            name: "Venusaur",
+            type: "Grass / Poison",
+            hp: 80,
+            attack: 82,
+            defense: 83
+        }
+    ]
+
+
+    res.render('home', {
+        pokemondbArray
+    })
+
+    // sort function by pokemon number not by ID
+    // pokemondbArray.sort(function (a, b) {
+    //     return parseInt(a.number, 10) - parseInt(b.number, 10);
+    // })
 }
 
 module.exports = homev;
