f1474ba34f3c61ce82bdc8e2ba15b63c1f4c5173
Edison K <koedison@gmail.com>
2020-03-29 18:43:25 -0700

    add admin and update stytling to differ admin and home pages

--- final_project/pokemondb/index.js
+++ final_project/pokemondb/index.js
@@ -6,11 +6,15 @@ const port = 3000
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
+
 // modules
 const homev = require ('./routes/home')
 const aboutv = require ('./routes/about')
+const adminv = require ('./routes/admin')
+
 
 app.get('/', homev)
 app.get('/about', aboutv)
+app.get('/admin', adminv)
 
-app.listen(port, () => console.log(`Pokemon database running on port ${port}!`))
+app.listen(port, () => console.log(`Pokemon database is running on port ${port}!`))
