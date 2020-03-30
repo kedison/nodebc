9e3ef55d163695739fa5ec6aa6ff417ada58abaa
Edison K <koedison@gmail.com>
2020-03-29 20:37:08 -0700

    add mongoose to admin

--- final_project/pokemondb/routes/admin/index.js
+++ final_project/pokemondb/routes/admin/index.js
@@ -1,37 +1,27 @@
-const indexv = (req, res) => {
+const indexv = async (req, res) => {
+    // mongoose implementation
+    const mongoose = require('mongoose');
+    const uri = "mongodb://pokemondb:pokemondb1234@cluster0-shard-00-00-gpfrf.mongodb.net:27017,cluster0-shard-00-01-gpfrf.mongodb.net:27017,cluster0-shard-00-02-gpfrf.mongodb.net:27017/pokemondb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
-    const pokemondbArray = [{
-            id: 1,
-            number: "001",
-            name: "Bulbasaur",
-            type: "Grass / Poison",
-            hp: 45,
-            attack: 49,
-            defense: 49
-        },
-        {
-            id: 2,
-            number: "012",
-            name: "Ivysaur",
-            type: "Grass / Poison",
-            hp: 60,
-            attack: 62,
-            defense: 63
-        },
-        {
-            id: 3,
-            number: "003",
-            name: "Venusaur",
-            type: "Grass / Poison",
-            hp: 80,
-            attack: 82,
-            defense: 83
-        }
-    ]
+    // mongoose model
+    const pokemondbStructure = mongoose.model('pokemondbStructure',{
+        number: String,
+        name: String,
+        type: String,
+        hp: Number,
+        attack: Number,
+        defense: Number
+    });
+
+    const pokemondb = await pokemondbStructure.find().exec();
 
 
     res.render('admin/index', {
-        pokemondbArray
+        pokemondb
     })
 
     // sort function by pokemon number not by ID
