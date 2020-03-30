4809bec6b16610886840f46cfbaaa66cf1fde00e
Edison K <koedison@gmail.com>
2020-03-29 22:54:17 -0700

    link database to home page, reflects the same data as admin page

--- final_project/pokemondb/routes/home.js
+++ final_project/pokemondb/routes/home.js
@@ -1,38 +1,16 @@
-const homev = (req, res) => {
+// mongoose implementation
+const mongoose = require('mongoose');
+const PokemondbStructure = require('../models/pokemondbStructure');
 
-    const pokemondbArray = [{
-            id: 1,
-            number: "001",
-            name: "Bulbasaur",
-            type: "Grass / Poison",
-            hp: 45,
-            attack: 49,
-            defense: 49
-        },
-        {
-            id: 2,
-            number: "012",
-            name: "Ivysaur",
-            type: "Grass / Poison",
-            hp: 60,
-            attack: 62,
-            defense: 63
-        },
-        {
-            id: 3,
-            number: "003",
-            name: "Venusaur",
-            type: "Grass / Poison",
-            hp: 80,
-            attack: 82,
-            defense: 83
-        }
-    ]
+const homev = async (req, res) => {
+    // mongodb://pokemondb:<password>@cluster0-shard-00-00-gpfrf.mongodb.net:27017,cluster0-shard-00-01-gpfrf.mongodb.net:27017,cluster0-shard-00-02-gpfrf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
+    let uri ="mongodb://pokemondb:pokemondb1234@cluster0-shard-00-00-gpfrf.mongodb.net:27017,cluster0-shard-00-01-gpfrf.mongodb.net:27017,cluster0-shard-00-02-gpfrf.mongodb.net:27017/pokdemondb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+ 
+    const pokemondb = await PokemondbStructure.find().exec();
 
-    res.render('home', {
-        pokemondbArray
-    })
+    res.render('home', { pokemondb })
 
     // sort function by pokemon number not by ID
     // pokemondbArray.sort(function (a, b) {
